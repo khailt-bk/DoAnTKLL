13cd99268a73a7385ae0a965f19a0162a6249ae4
Đặng Nam Thiện Nhân <101380123+Ruacontimnuoc@users.noreply.github.com>
2022-10-19 12:17:25 +0700

    Update main.py
    chuthich cac dong  code

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -14,21 +14,24 @@ cv2.waitKey(2000)
 # Su dung thuat toan
 lap = cv2.Laplacian(img, cv2.CV_64F, ksize=3)
 lap = np.uint8(np.absolute(lap))
+# Loc bộ lọc Sobel theo phuong ngang va phuong thang dung
 sobelX = cv2.Sobel(img, cv2.CV_64F, 1, 0)
-sobelY = cv2.Sobel(img, cv2.CV_64F, 0, 1)
-edges = cv2.Canny(img,100,150)
-
 sobelX = np.uint8(np.absolute(sobelX))
+# Loc bộ lọc Sobel theo phuong ngang va phuong thang dung
+sobelY = cv2.Sobel(img, cv2.CV_64F, 0, 1)
 sobelY = np.uint8(np.absolute(sobelY))
-
+# Loc bộ lọc Sobel theo phuong ngang va phuong thang dung
 sobelCombined = cv2.bitwise_or(sobelX, sobelY)
-
+# Bien so 100 va 150 co su tham doi
+# Ket qua theo loc Candy
+edges = cv2.Canny(img,100,150)
+# Tao file chua cac anh can xu ly
 titles = ['image', 'Laplacian', 'sobelX', 'sobelY', 'sobelCombined', 'Canny']
 images = [img, lap, sobelX, sobelY, sobelCombined, edges]
 
 for i in range(6):
+    # Chuyen anh ve file 8-bit de xu ly
     plt.subplot(2, 3, i+1), plt.imshow(images[i], 'gray')
     plt.title(titles[i])
     plt.xticks([]),plt.yticks([])
-
 plt.show()
